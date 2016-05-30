319fabb33199c52ea6ad8c4529ae54f1609beaa5
Madhusudan <madhusudhan638@gmail.com>
2016-05-30 17:10:52 +0000

    change in urls.py

--- form_builder/urls.py
+++ form_builder/urls.py
@@ -14,12 +14,17 @@ Including another URLconf
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import include, url
+from django.conf.urls import include, url patterns
 from django.contrib import admin
 from django.views.generic.base import RedirectView
+from .settings import STATIC_ROOT
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('forms.urls')),
     url(r'^$', RedirectView.as_view(url='/accounts')),
 ]
+
+urlpatterns += patterns('',
+ (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': STATIC_ROOT}),
+ )
